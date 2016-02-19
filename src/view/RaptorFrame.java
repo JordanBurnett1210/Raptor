0b3db6f7bd1fb764a5650d0fff0405a03d33f032
JordanBurnett1210 <jabur1210@gmail.com>
2016-02-19 09:48:49 -0700

    Finished setting up frame
    added details to setupFrame, added getters and setters.

--- src/view/RaptorFrame.java
+++ src/view/RaptorFrame.java
@@ -12,5 +12,37 @@ public class RaptorFrame extends JFrame
 	public RaptorFrame(TwoDController baseController)
 	{
 		this.baseController = baseController;
+		basePanel = new RaptorPanel(baseController);
+		setupFrame();
 	}
+	
+	public void setupFrame()
+	{
+		this.setContentPane(basePanel);
+		this.setSize(600, 600);
+		this.setTitle("Raptor");
+		this.setResizable(true);
+		this.setVisible(true);
+	}
+
+	public TwoDController getBaseController()
+	{
+		return baseController;
+	}
+
+	public void setBaseController(TwoDController baseController)
+	{
+		this.baseController = baseController;
+	}
+
+	public RaptorPanel getBasePanel()
+	{
+		return basePanel;
+	}
+
+	public void setBasePanel(RaptorPanel basePanel)
+	{
+		this.basePanel = basePanel;
+	}
+	
 }
